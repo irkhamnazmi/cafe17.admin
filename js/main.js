5c0f05590a5297e3b590c9f732f72101bf2e3765
Irkham Nazmi <irkham.nazmi@gmail.com>
2021-06-07 13:55:01 +0700

    update pertama

---
+++ js/main.js
@@ -0,0 +1,55 @@
+
+
+
+
+  $('#formModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    modal = $(this)
+   
+    //  modal.find('.modal-title').text(recipient)
+})
+
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {          
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {   
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+  
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+});
+
+
+$('#datepicker').datepicker({
+  weekStart: 1,
+  daysOfWeekHighlighted: "6,0",
+  autoclose: true,
+  todayHighlight: true,
+});
+$('#datepicker').datepicker("setDate", new Date());
+
+
+
+
+
