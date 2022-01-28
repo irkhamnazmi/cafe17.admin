b613b5eadc51405238a903aa56fd79402dbd3bb6
irkhamnazmi <irkham.nazmi@gmail.com>
2022-01-28 10:28:33 +0700

    Update Terbaru

--- app/core/Flasher.php
+++ app/core/Flasher.php
@@ -4,7 +4,7 @@ class Flasher
 {
     public static function setFlash($pesan, $aksi, $tipe)
     {
-        $_SESSION['flash'] = [
+        $_SESSION['flash_cafe17_admin'] = [
             'pesan' => $pesan,
             'aksi' => $aksi,
             'tipe' => $tipe
@@ -14,14 +14,14 @@ class Flasher
 
     public static function flash()
     {
-        if (isset($_SESSION['flash'])) {
+        if (isset($_SESSION['flash_cafe17_admin'])) {
             echo '<div class="alert alert-' . $_SESSION['flash']['tipe'] . ' alert-dismissible fade show" role="alert">
             <strong>' . $_SESSION['flash']['pesan'] . '</strong> ' . $_SESSION['flash']['aksi'] . '
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>';
-            unset($_SESSION['flash']);
+            unset($_SESSION['flash_cafe17_admin']);
         }
     }
 
